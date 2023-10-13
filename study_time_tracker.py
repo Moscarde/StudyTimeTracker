bf6eec345cbdf9fe309474c51cc33cf3d14db5ad
Gabriel Moscarde <78855746+Moscarde@users.noreply.github.com>
2023-10-13 15:42:15 -0300

    clean unnecessary code

--- study_time_tracker.py
+++ study_time_tracker.py
@@ -1,9 +1,6 @@
 from datetime import datetime, timedelta
 import os
 
-today = datetime.now().strftime("%Y-%m-%d")
-
-
 def read_today(today):
     if not os.path.exists(f'{today}.txt'):
         file = open(f'{today}.txt', 'w')
@@ -22,13 +19,12 @@ def calc_elapsed_time(stored, studied):
     return elapsed_time
 
 
-def write_today(study_duration):
-    global block
+def write_today(today, study_duration):
     today_data = read_today(today)
 
     today_data[1] = calc_elapsed_time(today_data[1], study_duration)
     today_data.append(
-        f"Date {datetime.now().strftime('%m/%d/%Y, %H:%M:%S')} - Time Spent {str(study_duration).split('.')[0]}")
+        f"Date {datetime.now().strftime('%m/%d/%Y, %H:%M:%S')} - Time Spent {round_delta(study_duration)}")
 
     today_data_string = '\n'.join(str(line) for line in today_data)
 
@@ -37,27 +33,33 @@ def write_today(study_duration):
     file.close()
 
 
-def start_menu():
+def round_delta(x): return str(x).split('.')[0]
+
+
+def main():
     print('Starting timer...')
     while True:
+        today = datetime.now().strftime("%Y-%m-%d")
         startup_time = datetime.now()
 
-        stop = input('Press enter to stop the timer')
+        stop = input('Press enter to stop the timer...')
         elapsed_time = datetime.now() - startup_time
 
-        print(f'Elapsed time: {elapsed_time}')
-        focused_multiplier = int(input('On a scale of 0 to 10, how focused were you during those time:'))/10
+        print(f'Elapsed time: {round_delta(elapsed_time)}')
+        focused_multiplier = int(
+            input('On a scale of 0 to 10, how focused were you during those time:'))/10
         final_elapsed_time_in_seconds = focused_multiplier * elapsed_time.total_seconds()
         final_elapsed_time = timedelta(seconds=final_elapsed_time_in_seconds)
 
-        write_today(final_elapsed_time)
-        print(f'{final_elapsed_time} has been written to {today}.txt')
+        write_today(today, final_elapsed_time)
+        print(f"{round_delta(final_elapsed_time)} has been written to {today}.txt")
 
-        continue_execution = input('Press [Y] to start a new timer or any other key to stop: ').upper()
+        continue_execution = input(
+            'Press [Y] to start a new timer or any other key to stop: ').upper()
         if continue_execution != 'Y':
             print('Stopping timer...')
             break
         print('Starting new timer...')
 
 
-start_menu()
+main()
